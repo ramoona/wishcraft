0c0c16cd332bacb667041c665ade9eb3d0d91502
ramoona <linda.rian.mail@gmail.com>
2025-05-04 19:44:41 +0200

    add empty state for foreign wishlist

--- src/components/wishlist/foreign/ForeignWishlist.tsx
+++ src/components/wishlist/foreign/ForeignWishlist.tsx
@@ -5,6 +5,7 @@ import { OtherUser } from "~/services/user/types";
 import { UserDetails } from "~/components/ui/user";
 import * as React from "react";
 import { FriendActionButtons } from "~/components/friends/FriendActionButtons";
+import { EmptyList } from "~/components/ui/emptyList";
 
 export function ForeignWishlist({
   wishlist,
@@ -17,16 +18,20 @@ export function ForeignWishlist({
 }) {
   return (
     <>
-      <div>
+      <div className="flex flex-col">
         <UserDetails user={owner} sticky />
-        <div className="mx-auto w-full max-w-lg bg-muted px-4 pt-4 shadow-[0_-10px_0_5px_#fff] sm:rounded-t">
-          <WishlistItems>
-            {wishlist.wishes.length === 0 ? (
-              <div>Nothing here yet</div>
-            ) : (
-              wishlist.wishes.map(wish => <ForeignWish data={wish} key={wish.id} username={owner.username} />)
-            )}
-          </WishlistItems>
+        <div className="mx-auto w-full max-w-lg grow bg-muted px-4 pt-4 shadow-[0_-10px_0_5px_#fff] sm:rounded-t">
+          {wishlist.wishes.length === 0 ? (
+            <div className="mx-auto flex h-full items-center justify-center">
+              <EmptyList shape="4">No wishes yet...</EmptyList>
+            </div>
+          ) : (
+            <WishlistItems>
+              {wishlist.wishes.map(wish => (
+                <ForeignWish data={wish} key={wish.id} username={owner.username} />
+              ))}
+            </WishlistItems>
+          )}
         </div>
       </div>
       {isLoggedIn ? <FriendActionButtons friendId={owner.id} isFriend={owner.isFriend} /> : null}
