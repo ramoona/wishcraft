85303c63953ddd2a75f1027f6d14e76627310b05
ramoona <linda.rian.mail@gmail.com>
2025-06-18 13:35:36 +0200

    fix async params

--- src/app/privacy-policy/[locale]/page.tsx
+++ src/app/privacy-policy/[locale]/page.tsx
@@ -6,8 +6,11 @@ import { ContentLayout } from "~/components/layout/Layout";
 import { languages, SupportedLanguages } from "~/lib/i18n/settings";
 import { PrivacyPolicyLanguageSwitcher } from "~/app/privacy-policy/[locale]/LanguageSwitcher";
 
-export default async function PrivacyPolicyPage({ params }: { params: { locale: string } }) {
-  const locale = languages.includes(params.locale as SupportedLanguages) ? (params.locale as SupportedLanguages) : "en";
+export default async function PrivacyPolicyPage({ params }: { params: Promise<{ locale: string }> }) {
+  const paramsResolved = await params;
+  const locale = languages.includes(paramsResolved.locale as SupportedLanguages)
+    ? (paramsResolved.locale as SupportedLanguages)
+    : "en";
   const filePath = path.join(process.cwd(), "content", "privacy-policy", `${locale}.md`);
   const file = await readFile(filePath, "utf8");
   const { content } = matter(file);
